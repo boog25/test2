7814b24f2ebff16f06b676bc5a86c4ed5f34321e
Voroshilov Dmitry <d.voroshilov@evotor.ru>
2022-04-21 22:37:33 +0500

    Initial commit

---
+++ src/MainClassTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+public class MainClassTest extends MainClass {
+
+    @Test
+    public void testGetClassNumber() {
+        int a = this.getClassNumber();
+        if (a > 45) {
+            System.out.println("True");
+        } else {
+            System.out.println("Value is not 45");
+
+        }
+
+    }
+
+}
